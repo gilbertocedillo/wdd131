7d2356cc0f9357e63f616677d746e7d767890e6c
gilbertocedillo <gilbertoabisaid00@gmail.com>
2024-05-02 18:04:48 -0600

    Complete

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,5 @@
 // select the DOM elements for output
 const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-const short = document.querySelector("#short");
-const medium = document.querySelector("#medium");
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const day = document.querySelector("#day");
-const dayofweek = document.querySelector("#dayofweek");
 
 // use the date object
 const today = new Date();
